66f5e3d1b9cf378fda310a38b3b98f8e4bac87e2
Akshat Jaiman <akshatjaiman121@gmail.com>
2022-07-24 22:27:44 +0530

    some changes

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.pokemon.com/us/pokedex/pichu"
           target="_blank"
           rel="noopener noreferrer"
         >
