22068d599acbe0ab0c8d1247a907b1a151598540
Akshat Jaiman <akshatjaiman121@gmail.com>
2022-07-24 22:26:10 +0530

    Added new packages

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hiii Lolooo
+          Hi Pichu
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Pika pika
         </a>
       </header>
     </div>
