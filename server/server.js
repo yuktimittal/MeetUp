6b1f4cae7c3d9f80d4f3ec17eb49f3aaae28df74
Yukti Mittal <yukti.mittal@hashedin.com>
2022-08-01 17:04:01 +0530

    connected with mongodb cluster

--- server/server.js
+++ server/server.js
@@ -1,4 +1,11 @@
-const express = require('express');
+const express = require("express");
+require("./config/db");
+
 const app = express();
 
 app.use(express.json());
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`server started at port ${port}`);
+});
