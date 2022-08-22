e3faa3b3fc2658d7dedb8d2ab872fed856a7eb57
Akshat Jaiman <akshatjaiman121@gmail.com>
2022-08-22 19:55:03 +0530

    updated models

--- server/models/Interest.js
+++ server/models/Interest.js
@@ -1,8 +1,19 @@
-var interestSchema = mongoose.Schema({
-    event : [Event.schema],
-    user: [User.schema],
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const interestSchema = mongoose.Schema({
+    event : {
+        type: Schema.Types.ObjectId,
+        ref: "Event",
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
     created_on : {
-        type: [Date],
-        default: ''
+        type: Date,
+        default: Date.now
     } 
-})
+});
+
+module.exports = mongoose.model("Group", GroupSchema);
--- server/models/Registration.js
+++ server/models/Registration.js
@@ -1,8 +1,19 @@
-var registrationSchema = mongoose.Schema({
-    event : [Event.schema],
-    user: [User.schema],
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const registrationSchema = mongoose.Schema({
+    event : {
+        type: Schema.Types.ObjectId,
+        ref: "Event",
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
     created_on : {
-        type: [Date],
-        default: ''
+        type: Date,
+        default: Date.now
     } 
-})
+});
+
+module.exports = mongoose.model("Group", GroupSchema);
