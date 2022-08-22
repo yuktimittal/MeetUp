b1001677be4f60cf3021d4544c8fb7076e01079b
Akshat Jaiman <akshatjaiman121@gmail.com>
2022-08-22 22:16:58 +0530

    Prettier :\

--- server/models/Group.js
+++ server/models/Group.js
@@ -2,28 +2,28 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const GroupSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    about: {
-        type: String
-    },
-    picture: {
-        type: String
-    },
-    events : {
-        type: Schema.Types.ObjectId,
-        ref: "Event",
-    },
-    users: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-    },
-    createdOn : {
-        type: Date,
-        default: Date.now
-    } 
+  name: {
+    type: String,
+    required: true,
+  },
+  about: {
+    type: String,
+  },
+  picture: {
+    type: String,
+  },
+  events: {
+    type: Schema.Types.ObjectId,
+    ref: "Event",
+  },
+  users: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  createdOn: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model("Group", GroupSchema);
--- server/models/Registration.js
+++ server/models/Registration.js
@@ -2,18 +2,18 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const registrationSchema = mongoose.Schema({
-    event : {
-        type: Schema.Types.ObjectId,
-        ref: "Event",
-    },
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-    },
-    createdOn : {
-        type: Date,
-        default: Date.now
-    } 
+  event: {
+    type: Schema.Types.ObjectId,
+    ref: "Event",
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  createdOn: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model("Registration", RegistrationSchema);
