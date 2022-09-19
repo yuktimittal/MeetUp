9bf66ec695592c8b24583b4ec06ce1aec9859834
Yukti Mittal <yukti.mittal@hashedin.com>
2022-09-19 23:03:13 +0530

    updated routes from header

--- client/src/config/Routes/index.js
+++ client/src/config/Routes/index.js
@@ -1,3 +1,5 @@
+import Login from 'login';
+import SignUp from 'login/SignUp';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Events from 'views/Events';
@@ -6,6 +8,10 @@ const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Events />} exact />
+      <Route path="/signUp" element={<SignUp />} exact />
+      <Route path="/login" element={<Login />} exact />
+      <Route path="/events" element={<Events />} exact />
+      <Route path="/groups" element={<Login />} exact />
     </Routes>
   );
 };
