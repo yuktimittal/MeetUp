d7f58ee560efa6d6b541567f343bde105aa782b1
Yukti Mittal <yukti.mittal@hashedin.com>
2022-11-25 00:11:21 +0530

    login-page-style

---
+++ client/src/login/constants.js
@@ -0,0 +1,19 @@
+export const DONT_HAVE_ACCOUNT = "Don't have an account?";
+export const REGISTER = 'Register';
+export const LOGIN = 'Login';
+
+export const WELCOME_BACK_TEXT = 'Welcome back, Please login to your account.';
+
+export const LOGIN_INPUT_LABELS = {
+  EMAIL: 'Email',
+  PASSWORD: 'Password',
+};
+
+export const LOGIN_KEYS = {
+  EMAIL: 'email',
+  PASSWORD: 'password',
+};
+
+export const LOGIN_SUCCESS_MSG = 'successfully logged in';
+
+export const USER = 'user';
