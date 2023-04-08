8c4407e11dd8e1c0adfd69bbb98102de68a79c39
Yukti Mittal <yukti.mittal@hashedin.com>
2023-04-09 01:08:08 +0530

    imporved sign up page UI

--- client/src/login/constants.js
+++ client/src/login/constants.js
@@ -1,19 +1,32 @@
 export const DONT_HAVE_ACCOUNT = "Don't have an account?";
 export const REGISTER = 'Register';
 export const LOGIN = 'Login';
+export const SIGN_UP = 'Sign Up';
 
 export const WELCOME_BACK_TEXT = 'Welcome back, Please login to your account.';
+export const CREATE_ACCOUNT_TEXT = 'Welcome to Mulakaat, Create your account.';
 
 export const LOGIN_INPUT_LABELS = {
   EMAIL: 'Email',
   PASSWORD: 'Password',
+};
+export const SIGNUP_INPUT_LABELS = {
+  EMAIL: 'Email',
+  PASSWORD: 'Password',
+  NAME: 'Name',
 };
 
 export const LOGIN_KEYS = {
   EMAIL: 'email',
   PASSWORD: 'password',
 };
+export const SIGNUP_KEYS = {
+  EMAIL: 'email',
+  PASSWORD: 'password',
+  NAME: 'name',
+};
 
 export const LOGIN_SUCCESS_MSG = 'successfully logged in';
+export const REGISTRATION_SUCCESS_MSG = 'User registered successully';
 
 export const USER = 'user';
