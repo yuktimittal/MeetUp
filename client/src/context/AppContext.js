483910cbac34d0295815b14f369890bb839c4cda
Yukti Mittal <yukti.mittal@hashedin.com>
2023-08-03 21:05:09 +0530

    fixed signup route issue

--- client/src/context/AppContext.js
+++ client/src/context/AppContext.js
@@ -1,16 +1,17 @@
 import React, { useState, useEffect, createContext } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 const AppContext = createContext();
 const AppContextProvider = ({ children }) => {
   const navigate = useNavigate();
+  const location = useLocation();
   const [user, setUser] = useState({});
 
   useEffect(() => {
     const userInfo = JSON.parse(localStorage.getItem('user'));
     setUser(userInfo);
 
-    if (!userInfo) {
+    if (!userInfo && location.pathname !== '/signUp') {
       navigate('/');
     }
   }, [navigate]);
