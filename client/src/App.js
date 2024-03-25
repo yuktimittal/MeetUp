666e6b227909da7ee911169304a0cc41d1ee625e
Yukti Mittal <yuktimittal008@gmail.com>
2024-03-25 15:48:50 +0530

    created separate file for theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,11 @@
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { teal } from '@mui/material/colors';
-import Header from './components/Header';
-import AppRoutes from 'config/Routes';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { AppContextProvider } from 'context/AppContext';
+import { ThemeProvider } from "@mui/material/styles";
+import Header from "./components/Header";
+import AppRoutes from "config/Routes";
+import { BrowserRouter as Router } from "react-router-dom";
+import { AppContextProvider } from "context/AppContext";
+import theme from "assets/theme";
 
 function App() {
-  const theme = createTheme({
-    typography: {
-      "fontFamily": `"Roboto", "Helvetica", "Arial", sans-serif`,
-    },
-    palette: {
-      primary: {
-        main: teal[300],
-      },
-      secondary: {
-        main: '#f44336',
-      },
-      buttons: {
-        main: '#799496',
-      },
-      black: {
-        main: '#000',
-      },
-    },
-  });
   return (
     <Router>
       <AppContextProvider>
